7be5b08e528f8ed789f51e893c07da3ef04e09ef
Andrey <omelyanukandrey@gmail.com>
2023-02-01 23:58:30 +0800

    in progress

--- intelas.sh
+++ intelas.sh
@@ -9,12 +9,18 @@
 cd ~/projects/intelas
 #find . -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} pull \;
 
-cd ~/projects/intelas/intelas
-tmux new-session -s intelas -n docker -d 'docker-compose up' 
-tmux new-window  -t intelas:1 -n back  'sleep 30s; docker container attach intelas_back_1'
-tmux new-window  -t intelas:2 -n front 'sleep 30s; docker container attach intelas_front_1'
-tmux new-window  -t intelas:3 -n sh-back  'sleep 30s; docker-compose exec back sh'
-tmux new-window  -t intelas:4 -n sh-front 'sleep 30s; docker-compose exec front sh'
+cd ~/projects/intelas/intelas-web
+tmux new-session  -s intelas   -n docker -d 'make web' 
+tmux new-window   -t intelas:1 -n back      'sleep 20s; docker container attach intelas-web_back_1'
+tmux split-window -v                        'sleep 20s; make sh s=back'
+tmux new-window   -t intelas:2 -n front     'sleep 20s; docker container attach intelas-web_front_1'
+tmux split-window -v                        'sleep 20s; make sh s=front'
+#tmux new-window   -t intelas:3 -n api      'sleep 10s; docker container attach intelas_api_1'
+#tmux new-window  -t intelas:4 -n worker   'sleep 10s; docker container attach intelas_worker_1'
+#tmux new-window  -t intelas:5 -n beat     'sleep 10s; docker container attach intelas_beat_1'
+#tmux new-window  -t intelas:6 -n flower   'sleep 10s; docker container attach intelas_flower_1'
+#tmux new-window  -t intelas:7 -n db       'sleep 10s; docker container attach intelas_db_1'
+#tmux new-window  -t intelas:8 -n redis    'sleep 10s; docker container attach intelas_redis_1'
 tmux select-window -t intelas:0
 tmux -2 attach-session -t intelas 
 
