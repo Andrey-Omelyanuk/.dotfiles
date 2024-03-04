954d36600a1a0ba1c2ad1910db7a7ef789964bf9
Andrey Omelyanuk <omelyanukandrey@gmail.com>
2024-03-04 12:08:02 +0300

    removed old files

--- reset.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-rm -rf ~/.WebStorm*/config/eval
-rm -rf ~/.java/.userPrefs/jetbrains/webstorm
-sed -i '/evlsprt/d' ~/.WebStorm*/config/options/options.xml
-sed -i '/evlsprt/d' ~/.java/.userPrefs/prefs.xml
-find ~/.WebStorm* -type d -exec touch -t $(date +"%Y%m%d%H%M") {} +;
-find ~/.WebStorm* -type f -exec touch -t $(date +"%Y%m%d%H%M") {} +;
